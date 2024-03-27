f8bc2fee0d4ac1a9032e0f9cb84015307dbe643d
55yong <dhdydwns988@gmail.com>
2024-03-27 15:32:43 +0900

    수업종료 ><

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed = 5.0f;
+    public GameObject explosionFactory;
+    float angle;
 
     Vector3 dir;
 
@@ -13,12 +15,15 @@ public class Enemy : MonoBehaviour
         int randValue = Random.Range(0, 10);
         GameObject target = GameObject.Find("Player");
 
-        if (randValue < 3)
+        if (randValue < 3 && target != null)
         {
+            angle = Mathf.Atan2(target.transform.position.y - transform.position.y, target.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle + 90);
             dir = target.transform.position - transform.position;
             dir.Normalize();
         }
-        else {
+        else
+        {
             dir = Vector3.down;
         }
 
@@ -31,6 +36,9 @@ public class Enemy : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        GameObject explosion = Instantiate(explosionFactory);
+        explosion.transform.position = this.transform.position;
+
         Destroy(this.gameObject);
         Destroy(collision.gameObject);
     }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,6 +8,15 @@ public class PlayerMove : MonoBehaviour
 
     void FixedUpdate()
     {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+
+        if (pos.x < 0f) pos.x = 0f;
+        if (pos.x > 1f) pos.x = 1f;
+        if (pos.y < 0f) pos.y = 0f;
+        if (pos.y > 1f) pos.y = 1f;
+
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
